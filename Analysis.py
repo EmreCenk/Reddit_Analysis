3b1fd1ad678932f9bcc892cf923f77cfa594a1e6
Emre Cenk <emrecenk9@gmail.com>
2021-09-24 19:27:03 -0400

    wrote function to convert all memes to text

--- Analysis.py
+++ Analysis.py
@@ -15,3 +15,19 @@ def get_text(image_path):
     except:
         return -1
 
+def convert_all_memes_to_text(meme_folder_path: str,
+                              export_path):
+    meme_names = os.listdir(meme_folder_path)
+
+    for i in range(len(meme_names)):
+        current_path = os.path.join(meme_folder_path, meme_names[i])
+        converted_string = get_text(current_path)
+
+        file = open(os.path.join(export_path, meme_names[i].replace(".png",".txt")), "a+", encoding = "utf-8")
+        file.write(converted_string)
+        file.close()
+        print(i, "/", len(meme_names))
+
+print(convert_all_memes_to_text(os.path.join("scraping", "auto_download0"),
+                                os.path.join("scraping", "exports")))
+
