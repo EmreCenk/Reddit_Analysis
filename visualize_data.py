7fb46fe2544bff70ed3885e853be8f5b0078085f
Emre Cenk <emrecenk9@gmail.com>
2021-10-01 23:59:07 -0400

    created matplotlib template for bar chart of word distribution

---
+++ visualize_data.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+x = ['a', 'b', 'c', 'd', 'e', 'f']
+y = [1, 2, 3, 4, 5, 6]
+
+x_pos = [i for i in range(len(x))]
+print(x_pos)
+
+plt.bar(x_pos, y, color='green')
+plt.xlabel("Word")
+plt.ylabel("Number of Times Used in Memes")
+plt.title("Word Usage in Top Memes Posted in r/Memes")
+
+plt.xticks(x_pos, x)
+
+plt.show()
